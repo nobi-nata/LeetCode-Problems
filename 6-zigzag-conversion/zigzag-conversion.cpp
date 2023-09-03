3d7026278c132b8dc617442ad53a449e9be342be
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-03 16:25:02 +0530

    Time: 6 ms (86.82%) | Memory: 10.5 MB (54.58%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.cpp
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        int i = 0;
-        int n = s.size();
-        vector<string> v(numRows);
-        while(i < n){
-            for(int j = 0; j < numRows && i < n; j++){
-                v[j].push_back(s[i]);
-                i++;
-            }
-            for(int j = numRows-2 ; j > 0 && i < n; j--){
-                v[j].push_back(s[i]);
-                i++;
-            }
+        // int i = 0;
+        // int n = s.size();
+        // vector<string> v(numRows);
+        // while(i < n){
+        //     for(int j = 0; j < numRows && i < n; j++){
+        //         v[j].push_back(s[i]);
+        //         i++;
+        //     }
+        //     for(int j = numRows-2 ; j > 0 && i < n; j--){
+        //         v[j].push_back(s[i]);
+        //         i++;
+        //     }
+        // }
+        // string ans = "";
+        // int k = 0;
+        // while(k < numRows){
+        //     ans += v[k];
+        //     k++;
+        // }
+        // return ans;
+
+
+        if(numRows <= 1) return s;
+        vector<string>v(numRows, ""); 
+        int j = 0, dir = -1;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+            if(j == numRows - 1 || j == 0) dir *= (-1); 
+            v[j] += s[i];
+            if(dir == 1) j++;
+            else j--;
         }
-        string ans = "";
-        int k = 0;
-        while(k < numRows){
-            ans += v[k];
-            k++;
-        }
-        return ans;
+
+        string res;
+        for(auto it : v) res += it; 
+        return res;
     }
 };
