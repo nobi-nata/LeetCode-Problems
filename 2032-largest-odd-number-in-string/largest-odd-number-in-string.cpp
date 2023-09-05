c06bf51ad1bc7cd238b28334f09ad8b9606e0d1b
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-05 13:30:32 +0530

    Time: 36 ms (9.01%) | Memory: 16.3 MB (8.33%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     string largestOddNumber(string num) {
-        deque<int> d;
-        string ans;
-        for(auto i : num){
-            d.push_back(i-'0');
-        }
-        while(!d.empty()){
-            if(d.back()%2){
-                break;
-            }
-            d.pop_back();
-        }
+        // deque<int> d;
+        // string ans;
+        // for(auto i : num){
+        //     d.push_back(i-'0');
+        // }
+        // while(!d.empty()){
+        //     if(d.back()%2){
+        //         break;
+        //     }
+        //     d.pop_back();
+        // }
 
-        while(!d.empty()){
-            ans += (d.front()+'0');
-            d.pop_front();
+        // while(!d.empty()){
+        //     ans += (d.front()+'0');
+        //     d.pop_front();
+        // }
+        // return ans;
+
+        int e = num.size()-1;
+        string ans = "";
+        while(e >= 0){
+            if((num[e]-'0')%2) break;
+            e--;
+        }
+        int i = 0;
+        while(i <= e){
+            ans += num[i];
+            i++;
         }
         return ans;
     }
