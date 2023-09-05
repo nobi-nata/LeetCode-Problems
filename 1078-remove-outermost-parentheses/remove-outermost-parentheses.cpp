dc2e53e03ae52eb90d06499d9a12914fbae9e4d8
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-05 12:43:51 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (82.25%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
+        // string ans;
+        // stack<int> st;
+        // for(auto i : s){
+        //     if(i == '('){
+        //         if(!st.empty()){
+        //             ans.push_back(i);
+        //         }
+        //         st.push(i);
+        //     }
+        //     else{
+        //         st.pop();
+        //         if(!st.empty()){
+        //             ans.push_back(i);
+        //         }
+        //     }
+        // }
+        // return ans;
+
         string ans;
-        stack<int> st;
-        for(auto i : s){
-            if(i == '('){
-                if(!st.empty()){
-                    ans.push_back(i);
-                }
-                st.push(i);
+        int c = 0;
+        for(int i : s){
+            if(c == 0){
+                c--;
+                continue;
             }
-            else{
-                st.pop();
-                if(!st.empty()){
-                    ans.push_back(i);
-                }
-            }
+
+
+            if(i == '(') c--;
+            else c++;
+
+            if(c == 0) continue;
+
+            ans.push_back(i);
         }
         return ans;
     }
