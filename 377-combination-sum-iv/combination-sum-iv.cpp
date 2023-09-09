5fe8ff4f05c68568d838c6608427187b9473a22f
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-09 08:54:51 +0530

    Time: 3 ms (50.37%) | Memory: 6.6 MB (46.24%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -2,25 +2,28 @@ class Solution {
 public:
 
     int cnt(vector<int>& nums, int target, vector<int> &dp){
-        
+        // If the remaining target is 0, there's one valid combination.
         if(target == 0) return 1;
+        // If the remaining target becomes negative, it's not possible to reach it.
         if(target < 0) return 0;
-        
+         // If the result for 'remainingTarget' is already computed, return it.
         if(dp[target] != -1) return dp[target];
 
         int count = 0;
 
         for(int i = 0; i < nums.size(); i++){
             int n = nums[i];
+            // recursively calculate combinations for the new target.
             count += cnt(nums, target - n, dp);
         }
+        // Store and return the computed result.
 
         return dp[target] = count;
     }
 
     int combinationSum4(vector<int>& nums, int target) {
         
-        vector<int> dp(1001, -1);
+        vector<int> dp(target + 1, -1);
         return cnt(nums, target, dp);
     }
 };
