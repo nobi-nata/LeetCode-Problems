37dfec760e9dc45fcbd9c166f039fe230f9a3f3f
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-16 12:59:13 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (89.43%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> p = {0, 1};
-        n--;
-        int prod = 1;
-        while(n > 0){
-            prod = prod*2;
-            for(int i = p.size()-1; i >= 0; i--) p.push_back(p[i]+prod);
-            n--;
+        // vector<int> p = {0, 1};
+        // n--;
+        // int prod = 1;
+        // while(n > 0){
+        //     prod = prod*2;
+        //     for(int i = p.size()-1; i >= 0; i--) p.push_back(p[i]+prod);
+        //     n--;
+        // }
+        // return p;
+
+        vector<int> ans(1<<n);
+
+        for(int i = 0; i < (1<<n); i++){
+            ans[i] = i^(i>>1);
         }
-        return p;
+        return ans;
     }
 };
