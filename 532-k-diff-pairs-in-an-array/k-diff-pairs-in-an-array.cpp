c0d16daa3b336c7f40cbff57e2d0a683ddcbd7c1
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-18 12:18:51 +0530

    Time: 13 ms (73.88%) | Memory: 13.4 MB (24.94%) - LeetSync

--- 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/k-diff-pairs-in-an-array.cpp
@@ -1,39 +1,57 @@
 class Solution {
 public:
     int findPairs(vector<int>& nums, int k) {
+        // unordered_map<int, int> mp;
+        // vector<int> vec;
+        // for(auto i : nums){
+        //     mp[i]++;
+        // }
+        // int c = 0;
+        // for(auto i : mp){
+        //     vec.push_back(i.first);
+        //     if(i.second > 1) c++;
+        // }
+        // sort(vec.begin(), vec.end());
+        // int n = vec.size();
+        // if(k == 0){
+        //     return c;
+        // }
+        // if(n == 1) return 0;
+        // int ans = 0;
+
+        // int i = 0;
+        // int j = 1;
+
+        // while(j < n){
+        //     if(i!=j && (vec[j] - vec[i]) == k){
+        //         ans++;
+        //         j++;
+        //     }
+        //     else if(i!=j && (vec[j] - vec[i]) > k){
+        //         i++;
+        //     }
+        //     else{
+        //         j++;
+        //     }
+        // }
+        // return ans;
+
+
         unordered_map<int, int> mp;
-        vector<int> vec;
         for(auto i : nums){
             mp[i]++;
         }
-        int c = 0;
+        int cnt = 0;
         for(auto i : mp){
-            vec.push_back(i.first);
-            if(i.second > 1) c++;
-        }
-        sort(vec.begin(), vec.end());
-        int n = vec.size();
-        if(k == 0){
-            return c;
-        }
-        if(n == 1) return 0;
-        int ans = 0;
-
-        int i = 0;
-        int j = 1;
-
-        while(j < n){
-            if(i!=j && (vec[j] - vec[i]) == k){
-                ans++;
-                j++;
+            if(k == 0){
+                if(i.second > 1){
+                    cnt++;
+                }
             }
-            else if(i!=j && (vec[j] - vec[i]) > k){
-                i++;
-            }
-            else{
-                j++;
+            else if(mp.find(i.first+k) != mp.end()){
+                cnt++;
             }
         }
-        return ans;
+        return cnt;
     }
 };
