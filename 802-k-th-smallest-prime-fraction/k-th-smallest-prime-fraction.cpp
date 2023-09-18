8d6d63b2ea125f6042201d5f46b3f30df1734da8
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-18 16:04:01 +0530

    Time: 18 ms (88.90%) | Memory: 10.8 MB (81.63%) - LeetSync

--- 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -1,23 +1,81 @@
+// class Solution {
+// public:
+//     vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+//         int n = arr.size();
+
+//         priority_queue<pair<double, pair<int,int>> , vector<pair<double, pair<int,int>>>, greater<pair<double, pair<int,int>>> > pq;
+
+//         for(int i = 0; i < n; i++){
+//             for(int j = i+1; j < n; j++){
+//                 pq.push({(double)arr[i]/arr[j], {arr[i], arr[j]}});
+//             }
+//         }
+
+//         while(k > 1){
+//             pq.pop();
+//             k--;
+//         }
+//         int x = pq.top().second.first;
+//         int y = pq.top().second.second;
+
+//         return {x, y};
+//     }
+// };
+
+
+
 class Solution {
 public:
-    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
-        int n = arr.size();
-
-        priority_queue<pair<double, pair<int,int>> , vector<pair<double, pair<int,int>>>, greater<pair<double, pair<int,int>>> > pq;
-
-        for(int i = 0; i < n; i++){
-            for(int j = i+1; j < n; j++){
-                pq.push({(double)arr[i]/arr[j], {arr[i], arr[j]}});
+    int helper(float check, vector<int>& arr){
+        int left = 0, right = arr.size();
+        while(left<right){
+            int mid = left + (right-left)/2;
+            if(arr[mid]==check){
+                return mid;
+            }
+            else if(arr[mid]>check){
+                right = mid;
+            }
+            else{
+                left = mid+1;
             }
         }
-
-        while(k > 1){
-            pq.pop();
-            k--;
+        return right;
+    }
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        float left = 0, right = 1;
+        int n = arr.size();
+        while(true){
+            float mid = float((float(left)+float(right))/2);
+            vector<int> bounds;
+            for(int i=0;i<n;i++){
+                bounds.emplace_back(helper(float(arr[i])/mid,arr));
+            }
+            int curr_sum = 0;
+            for(auto b:bounds){
+                if(b<n) curr_sum += (n-b);
+            }
+            // cout<<curr_sum<<endl;
+            if(curr_sum > k){
+                right = mid;
+            }
+            else if(curr_sum < k){
+                left = mid;
+            }
+            else{
+                int a = 0, b = 0;
+                float mx = 0;
+                for(int i=0;i<bounds.size();i++){
+                    if(bounds[i]<n){
+                        if(float(arr[i])/float(arr[bounds[i]]) > mx){
+                            mx = float(arr[i])/float(arr[bounds[i]]);
+                            a = arr[bounds[i]]; b = arr[i];
+                        }
+                    }
+                }
+                return {b,a};
+            }
         }
-        int x = pq.top().second.first;
-        int y = pq.top().second.second;
-
-        return {x, y};
+        return {};
     }
 };
