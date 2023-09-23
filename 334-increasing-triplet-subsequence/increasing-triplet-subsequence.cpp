169b556dcdf4901bca19d8c60d974b28206c485b
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-23 20:21:09 +0530

    Time: 114 ms (15.63%) | Memory: 121.6 MB (6.54%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -42,16 +42,58 @@ public:
         // }
         // return false;
 
-        int a=INT_MIN,b=INT_MIN;
-        for(auto i:nums)
+        // int a=INT_MIN,b=INT_MIN;
+        // for(auto i:nums)
+        // {
+        //     if(a==INT_MIN or i <= a)
+        //         a=i;
+        //     else if(b==INT_MIN or i <= b)
+        //         b=i;
+        //     else
+        //         return true;
+        // }
+        // return false;
+
+        
+
+        int n = nums.size();
+        
+        // left_min[i] will store the minimum from left till ith
+
+        vector<int> left_min(n);
+        
+        // right_max[i] will store the maximum from right till ith
+
+        vector<int> right_max(n);
+        
+        // fill left_min array
+
+        left_min[0] = nums[0];
+
+        for(int i = 1; i < n; i++)
         {
-            if(a==INT_MIN or i <= a)
-                a=i;
-            else if(b==INT_MIN or i <= b)
-                b=i;
-            else
-                return true;
+            left_min[i] = min(left_min[i - 1], nums[i]);
         }
+        
+        // fill right_max array
+
+        right_max[n - 1] = nums[n - 1];
+
+        for(int i = n - 2; i >= 0; i--)
+        {
+            right_max[i] = max(right_max[i + 1], nums[i]);
+        }
+        
+        // check that is there any element which has smaller element on left side and greater element on right side
+
+        for(int i = 1; i < n - 1; i++)
+        {
+            if(left_min[i - 1] < nums[i] && nums[i] < right_max[i + 1])
+            {
+                return true;
+            }
+        }
+        
         return false;
 
     }
