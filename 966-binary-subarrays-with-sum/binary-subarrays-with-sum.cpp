61eef37be7ae03cc6400a704d584e5b936ba59bd
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-24 12:48:50 +0530

    Time: 31 ms (58.22%) | Memory: 31.2 MB (51.64%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,20 +1,42 @@
+// class Solution {
+// public:
+//     int numSubarraysWithSum(vector<int>& nums, int goal) {
+//         int n = nums.size();
+//         int i = 0;
+//         int cnt = 0;
+//         int ans = 0;
+//         unordered_map<int, int> mp;
+//         while(i < n){
+//             cnt += nums[i];
+//             if(cnt == goal) ans++;
+//             if(mp.find(cnt-goal) != mp.end()){
+//                 ans += mp[cnt-goal];
+//             }
+//             mp[cnt]++;
+//             i++;
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int n = nums.size();
-        int i = 0;
-        int cnt = 0;
-        int ans = 0;
-        unordered_map<int, int> mp;
-        while(i < n){
-            cnt += nums[i];
-            if(cnt == goal) ans++;
-            if(mp.find(cnt-goal) != mp.end()){
-                ans += mp[cnt-goal];
+    int helper(vector<int> nums, int goal){
+        int n = nums.size(), i = 0, j = 0, sum = 0, cnt = 0;
+
+        while(j < n){
+            sum += nums[j];
+            while(i <= j && sum > goal){
+                sum -= nums[i];
+                i++;
             }
-            mp[cnt]++;
-            i++;
+            cnt += j-i+1;
+            j++;
         }
-        return ans;
+        return cnt;
+    }
+    
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return helper(nums, goal) - helper(nums, goal-1);
     }
 };
