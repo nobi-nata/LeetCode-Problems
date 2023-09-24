c1a49a01640c06c89f5b0b76226e850c997b2356
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-24 14:15:11 +0530

    Time: 96 ms (84.91%) | Memory: 74.3 MB (39.75%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -1,23 +1,42 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
+    int helper(vector<int> nums, int k){
         int i = 0, j = 0, n = nums.size(), cnt = 0;
         int ans = 0;
-        unordered_map<int, int> mp;
+
         while(j < n){
-            if(nums[j] % 2) cnt++;
-            
-            if(mp.find(cnt-k) != mp.end()){
-                ans += mp[cnt-k];
+            if(nums[j] % 2){
+                cnt++;
             }
-            // while(i <= j && cnt > k){
-            //     if(nums[i] % 2) cnt--;
-            //     i++;
-            // }
-            if(cnt == k) ans++;
-            mp[cnt]++;
+            while(i <= j && cnt > k){
+                if(nums[i] % 2) cnt--;
+                i++;
+            }
+            ans += j-i+1;
             j++;
         }
         return ans;
+    }
+
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        // int i = 0, j = 0, n = nums.size(), cnt = 0;
+        // int ans = 0;
+        // unordered_map<int, int> mp;
+        // while(j < n){
+        //     if(nums[j] % 2) cnt++;
+            
+        //     if(mp.find(cnt-k) != mp.end()){
+        //         ans += mp[cnt-k];
+        //     }
+            
+        //     if(cnt == k) ans++;
+        //     mp[cnt]++;
+        //     j++;
+        // }
+        // return ans;
+
+
+
+        return helper(nums, k) - helper(nums, k-1);
     }
 };
