1e30114c0a7c65199186f701b81a7906660df105
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-25 09:21:07 +0530

    Time: 4 ms (48.11%) | Memory: 7.2 MB (8.68%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,  int> mp;
+        // unordered_map<char,  int> mp;
         
-        for(auto i : t){
-            mp[i]++;
+        // for(auto i : t){
+        //     mp[i]++;
+        // }
+        // for(auto i : s){
+        //     mp[i]--;
+        // }
+        // for(auto i : mp){
+        //     if(i.second == 1) return i.first;
+        // }
+        // return ' ';
+
+
+        char result = 0;
+        for (char c : s) {
+            result ^= c; 
         }
-        for(auto i : s){
-            mp[i]--;
+        
+        for (char c : t) {
+            result ^= c; 
         }
-        for(auto i : mp){
-            if(i.second == 1) return i.first;
-        }
-        return ' ';
+        
+        return result;
     }
 };
