90e8c2fa975d9fc2b241a8b6c2c8c5ebccd6c71e
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-26 09:27:45 +0530

    Time: 2 ms (59.76%) | Memory: 7 MB (17.68%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    
+
+    string removeDuplicateLetters(string s) {
+        unordered_map<char, int> mp;
+        for(auto i : s) mp[i]++;
+        vector<int> vis(26, 0);
+        string ans = "";
+
+        for(int i = 0; i < s.size(); i++){
+            mp[s[i]]--;
+            if(vis[s[i] - 'a']) continue;
+
+            while(!ans.empty() && ans.back() > s[i] && mp[ans.back()]){
+                vis[ans.back() - 'a'] = 0;
+                ans.pop_back();
+            }
+            ans.push_back(s[i]);
+            vis[s[i] - 'a'] = 1;
+        }
+        return ans;
+
+    }
+};
