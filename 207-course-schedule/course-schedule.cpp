47d891b678bd31c33ac348a3897dce809dae5b5d
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-26 13:25:34 +0530

    Time: 2869 ms (5.07%) | Memory: 743.6 MB (5.02%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>> adj, vector<int> &vis, int node,vector<int> &pvis){
+        vis[node] = 1;
+        pvis[node] = 1;
+        for(auto i : adj[node]){
+            if(!vis[i]){
+                if(!dfs(adj, vis, i, pvis)) return false;
+            }
+            else if(pvis[i]){
+                return false;
+            }
+        }
+        pvis[node] = 0;
+        return true;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        for(auto i : prerequisites){
+            adj[i[1]].push_back(i[0]);
+        }
+
+        vector<int> vis(numCourses, 0);
+
+        for(int i = 0; i < numCourses; i++){
+            if(!vis[i] && adj[i].size()){
+                vector<int> pvis(numCourses, 0);
+                if(!dfs(adj, vis, i, pvis)) return false;
+            }
+        }
+        return true;
+
+    }
+};
