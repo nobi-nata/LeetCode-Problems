db23355dae600db45620b6de4a43cea4e7428ef5
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-27 10:46:14 +0530

    Time: 206 ms (52.35%) | Memory: 70.1 MB (43.25%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.cpp
@@ -1,18 +1,37 @@
+// class Solution {
+// public:
+//     static bool cmp(string a, string b){
+//         if(a.size() == b.size()){
+//             if(a < b) return true;
+//             return false;
+//         }
+//         else if(a.size() < b.size()) return true;
+//         return false;
+//     }
+
+//     string kthLargestNumber(vector<string>& nums, int k) {
+//         sort(nums.begin(), nums.end(), cmp);
+//         int n = nums.size();
+
+//         return nums[n-k];
+//     }
+// };
+
 class Solution {
 public:
-    static bool cmp(string a, string b){
-        if(a.size() == b.size()){
-            if(a < b) return true;
-            return false;
-        }
-        else if(a.size() < b.size()) return true;
-        return false;
-    }
-
     string kthLargestNumber(vector<string>& nums, int k) {
-        sort(nums.begin(), nums.end(), cmp);
-        int n = nums.size();
+         auto comp = [](string  &a, string &b) {
+            if(a.size() == b.size()) {
+                return a > b;
+            }
+            return a.size() > b.size();
+        };
 
-        return nums[n-k];
+        priority_queue< string, vector<string>, decltype(comp) >pq(comp);
+        for(auto &it : nums) {
+            pq.push(it);
+            if(pq.size() > k)pq.pop();
+        }
+        return pq.top();
     }
 };
