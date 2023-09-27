7e51965c5acd9e5edf1de89d449a2921f6067c8c
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-09-27 10:09:25 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (11.71%) - LeetSync

---
+++ 916-decoded-string-at-index/decoded-string-at-index.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    std::string decodeAtIndex(string s, int k) {
+        long long length = 0;
+        int i = 0;
+        
+        while (length < k) {
+            if (isdigit(s[i])) {
+                length *= s[i] - '0';
+            } else {
+                length++;
+            }
+            i++;
+        }
+        cout << length << " ";
+        for (int j = i - 1; j >= 0; j--) {
+            if (isdigit(s[j])) {
+                length /= s[j] - '0';
+                k %= length;
+            } else {
+                if (k == 0 || k == length) {
+                    return std::string(1, s[j]);  // Convert char to std::string
+                }
+                length--;
+            }
+            cout << k << " "; 
+        }
+        
+        return ""; // Default return, should never reach here given problem constraints
+    }
+};
