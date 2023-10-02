e3c6478c495f73c226ad15250185d568fc654aba
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-02 10:31:08 +0530

    Time: 31 ms (74.54%) | Memory: 13.6 MB (23.75%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -2,22 +2,30 @@ class Solution {
 public:
     bool winnerOfGame(string colors) {
 
-        priority_queue<int> cntA;
-        priority_queue<int> cntB;
+        // priority_queue<int> cntA;
+        // priority_queue<int> cntB;
         int cnt = 0;
+        int cntofA = 0;
+        int cntofB = 0;
         for(int i = 0; i < colors.size(); i++){
             if(colors[i] == 'A') cnt++;
             else{
                 if(cnt){
-                    cout << cnt;
-                    cntA.push(cnt);
+                    // cout << cnt;
+                    if(cnt >= 3){
+                        cntofA += cnt-2;
+                    }
+                    // cntA.push(cnt);
                     cnt = 0;
                 }
             }
         }
-        if(cnt != 0){
-            cout << cnt;
-            cntA.push(cnt);
+        if(cnt){
+            // cout << cnt;
+            if(cnt >= 3){
+                cntofA += cnt-2;
+            }
+            // cntA.push(cnt);
             cnt = 0;
         }
         cnt = 0;
@@ -25,32 +33,59 @@ public:
             if(colors[i] == 'B') cnt++;
             else{
                 if(cnt != 0){
-                    cout << cnt;
-                    cntB.push(cnt);
+                    // cout << cnt;
+                    // cntB.push(cnt);
+                    if(cnt >= 3) cntofB += cnt-2;
                     cnt = 0;
                 }
             }
         }
         if(cnt != 0){
-            cout << cnt;
-            cntB.push(cnt);
-            cnt = 0;
+            if(cnt >= 3) cntofB += cnt-2;
+            cnt = 0;;
         }
-        int cntofA = 0;
-        int cntofB = 0;
+        // bool turn = true;
+        // while(true){
+        //     if(turn){
+        //         if(!cntA.empty() && cntA.top() >= 3){ 
+        //             int val = cntA.top();
+        //             cntA.pop();
+        //             cntA.push(val-1);
+        //             turn = false;
+        //             cout << 'a';
+        //         }
+        //         else return false;
+                
+        //     }
+        //     else{
+        //         if(!cntB.empty() && cntB.top() >= 3){ 
+        //             int val = cntB.top();
+        //             cntB.pop();
+        //             cntB.push(val-1);
+        //             turn = true;
+        //         }
+        //         else return true;
+        //         cout << 'a';
+        //     }
+        // }
+        // return true;
 
-        while(!cntA.empty() && cntA.top() >= 3){
-            int val = cntA.top();
-            cntA.pop();
-            cntofA += val-2;
-        }
-        while(!cntB.empty() && cntB.top() >= 3){
-            int val = cntB.top();
-            cntB.pop();
-            cntofB += val-2;
-        }
+
+
+        // int cntofA = 0;
+        // int cntofB = 0;
+
+        // while(!cntA.empty() && cntA.top() >= 3){
+        //     int val = cntA.top();
+        //     cntA.pop();
+        //     cntofA += val-2;
+        // }
+        // while(!cntB.empty() && cntB.top() >= 3){
+        //     int val = cntB.top();
+        //     cntB.pop();
+        //     cntofB += val-2;
+        // }
         if(cntofA > cntofB) return true;
         return false;
-        
     }
 };
