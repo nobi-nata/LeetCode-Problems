ba1e0cf0ff8b9df0d7a3cc3a04c8e211c2501a15
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-02 10:25:37 +0530

    Time: 163 ms (5.24%) | Memory: 20.5 MB (5.12%) - LeetSync

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -36,30 +36,21 @@ public:
             cntB.push(cnt);
             cnt = 0;
         }
-        bool turn = true;
-        while(true){
-            if(turn){
-                if(!cntA.empty() && cntA.top() >= 3){ 
-                    int val = cntA.top();
-                    cntA.pop();
-                    cntA.push(val-1);
-                    turn = false;
-                    cout << 'a';
-                }
-                else return false;
-                
-            }
-            else{
-                if(!cntB.empty() && cntB.top() >= 3){ 
-                    int val = cntB.top();
-                    cntB.pop();
-                    cntB.push(val-1);
-                    turn = true;
-                }
-                else return true;
-                cout << 'a';
-            }
+        int cntofA = 0;
+        int cntofB = 0;
+
+        while(!cntA.empty() && cntA.top() >= 3){
+            int val = cntA.top();
+            cntA.pop();
+            cntofA += val-2;
         }
-        return true;
+        while(!cntB.empty() && cntB.top() >= 3){
+            int val = cntB.top();
+            cntB.pop();
+            cntofB += val-2;
+        }
+        if(cntofA > cntofB) return true;
+        return false;
+        
     }
 };
