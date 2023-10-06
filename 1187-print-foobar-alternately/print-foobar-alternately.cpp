e21c4bd9e5ab776d76ed44ec7369c61a6ec63778
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-06 14:16:33 +0530

    Time: 4 ms (86.61%) | Memory: 8.8 MB (10.82%) - LeetSync

--- 1187-print-foobar-alternately/print-foobar-alternately.cpp
+++ 1187-print-foobar-alternately/print-foobar-alternately.cpp
@@ -1,46 +1,46 @@
 class FooBar {
 private:
     int n;
-    mutex m1, m2;
-    // condition_variable cv;
-    // int flag;
+    mutex m;
+    condition_variable cv;
+    int flag;
 public:
     FooBar(int n) {
         this->n = n;
-        // flag = 1;
-        m2.lock();
+        flag = 0;
+        // m2.lock();
     }
 
     void foo(function<void()> printFoo) {
-        // unique_lock<mutex> lock(m);
+        
         for (int i = 0; i < n; i++) {
+            unique_lock<mutex> lock(m);
+            while(flag == 1){
+                cv.wait(lock);
+            }
             
-            // while(flag == 0){
-            //     cv.wait(lock);
-            // }
-            
-            m1.lock();
+            // m1.lock();
         	// printFoo() outputs "foo". Do not change or remove this line.
         	printFoo();
-            // flag = 0;
-            // cv.notify_one();
-            m2.unlock();
+            flag = 1;
+            cv.notify_all();
+            // m2.unlock();
         }
     }
 
     void bar(function<void()> printBar) {
-        // unique_lock<mutex> lock(m);
+        
         for (int i = 0; i < n; i++) {
-            
-            // while(flag == 1){
-            //     cv.wait(lock);
-            // }
-            m2.lock();
+            unique_lock<mutex> lock(m);
+            while(flag == 0){
+                cv.wait(lock);
+            }
+            // m2.lock();
         	// printBar() outputs "bar". Do not change or remove this line.
         	printBar();
-            // flag = 0;
-            // cv.notify_one();
-            m1.unlock();
+            flag = 0;
+            cv.notify_all();
+            // m1.unlock();
         }
     }
 };
