641d28b23a2345713a5393537760b197c1ca89f3
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-08 17:50:19 +0530

    Time: 19 ms (84.32%) | Memory: 9.8 MB (99.49%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -1,19 +1,29 @@
 class Solution {
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+        int m = nums1.size();
+        int n = nums2.size();
         
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MIN));
+        // vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MIN));
         
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j <= m; ++j) {
-                dp[i][j] = max(nums1[i - 1] * nums2[j - 1] + max(dp[i - 1][j - 1], 0),
-                              max(dp[i - 1][j], dp[i][j - 1]));
+        // for (int i = 1; i <= n; ++i) {
+        //     for (int j = 1; j <= m; ++j) {
+        //         dp[i][j] = max(nums1[i - 1] * nums2[j - 1] + max(dp[i - 1][j - 1], 0),
+        //                       max(dp[i - 1][j], dp[i][j - 1]));
+        //     }
+        // }
+        
+        // return dp[n][m];
+
+        std::vector<int> current(n + 1, INT_MIN), previous(n + 1, INT_MIN);
+         for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                int curr_product = nums1[i-1] * nums2[j-1];
+                current[j] = std::max({curr_product, previous[j], current[j-1], curr_product + std::max(0, previous[j-1])});
             }
+            std::swap(current, previous);
         }
-        
-        return dp[n][m];
+        return previous[n];
 
     }
 };
