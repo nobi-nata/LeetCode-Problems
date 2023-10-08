066eb8490d2afa23ffa7e3c1efd299494ed6e04c
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-08 18:15:38 +0530

    Time: 23 ms (55.65%) | Memory: 26.5 MB (6.07%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        // Like insertion sort -> go to last k elements and bring them to start
+        // int n = nums.size();
+        // k = k%n;
+        
+        // for(int i = n - k; i < n; i++){
+        //     int j = i;
+        //     while(j > (i - n + k)){
+        //         swap(nums[j], nums[j-1]);
+        //         j--;
+        //     }
+        // }
+        
+        // using deque;
+        deque<int> dq;
+
+        for(int i : nums) dq.push_back(i);
+
+        while(k){
+            int ele = dq.back();
+            dq.pop_back();
+            dq.push_front(ele);
+            k--;
+        }
+        int j = 0;
+        for(auto i : dq){
+            nums[j++] = i;
+        }
+
+
+    }
+};
