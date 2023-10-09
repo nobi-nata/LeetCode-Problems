d82f7d5681f0e3fc685c3f95a09f66de679669f5
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-09 17:15:25 +0530

    Time: 15 ms (50.96%) | Memory: 19.9 MB (37.38%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -15,29 +15,29 @@ public:
 
         //TC: O(n) + SC: O(n);
 
-        int n = nums.size();
-        unordered_map<int, int> mp;
-        for(int i : nums){
-            mp[i]++;
-            if(mp[i] > n/2) return i;
-        }
-        return 0;
-        
-        // int ele = nums[0];
-        // int freq = 1;
-
-        // for(int i = 1; i < nums.size(); i++){
-        //     if(nums[i] != ele && freq){
-        //         freq--;
-        //     }
-        //     else if(nums[i] == ele){
-        //         freq++;
-        //     }
-        //     else{
-        //         ele = nums[i];
-        //         freq = 1;
-        //     }
+        // int n = nums.size();
+        // unordered_map<int, int> mp;
+        // for(int i : nums){
+        //     mp[i]++;
+        //     if(mp[i] > n/2) return i;
         // }
-        // return ele;
+        // return 0;
+
+        //TC: O(n)
+
+        int n = nums.size();
+        int freq = 1;
+        int majority = nums[0];
+        for(int i = 1; i < n; i++){
+            if(nums[i] == majority) freq++;
+            else freq--;
+            if(freq == 0){
+                freq = 1;
+                majority = nums[i];
+            }
+        }
+        return majority;
+        
+       
     }
 };
