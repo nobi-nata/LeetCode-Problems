bb250f2c53f0e693b1f5f72d1f7e4025a8ffacd6
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-09 19:25:27 +0530

    Time: 9 ms (61.49%) | Memory: 16.1 MB (78.17%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -20,59 +20,58 @@ public:
         
         //TC: O(n) + SC: O(n);
 
-        int n = nums.size();
-        unordered_map<int, int> mp;
-        vector<int> ans;
-        for(int i : nums){
-            mp[i]++;
-            if(mp[i] > n/3){ 
-                if(ans.size() == 0) ans.push_back(i);
-                else if(ans.back() != i) ans.push_back(i);
-            }
-            if(ans.size() > 1) return ans;
-        }
-        return ans;
-        
-        // int ele = nums[0];
-        // int freq = 1;
-        // int ele1 = nums[0];
-        // int freq1 = 0;
+        // int n = nums.size();
+        // unordered_map<int, int> mp;
+        // vector<int> ans;
+        // for(int i : nums){
+        //     mp[i]++;
+        //     if(mp[i] > n/3){ 
+        //         if(ans.size() == 0) ans.push_back(i);
+        //         else if(ans.back() != i) ans.push_back(i);
+        //     }
+        //     if(ans.size() > 1) return ans;
+        // }
+        // return ans;
 
-        // for(int i = 1; i < nums.size(); i++){
-        //     if(freq1 == 0 && ele != nums[i]){
-        //         ele1 = nums[i];
-        //         freq1 = 1;
-        //     }
-        //     else if(freq == 0 && ele1 != nums[i]){
-        //         ele = nums[i];
-        //         freq = 1;
-        //     }
-        //     else if(ele == nums[i]){
-        //         freq++;
-        //     }
-        //     else if(ele1 == nums[i]){
-        //         freq1++;
-        //     }
-        //     else{
-        //         freq--;
-        //         freq1--;
-        //     }
-            
-        // }
-        // int cnt = 0, cnt1 = 0;
-        // for(auto i : nums){
-        //     if(i == ele) cnt++;
-        //     if(i == ele1) cnt1++;
-        // }
-        // if(cnt > nums.size()/3 && freq && freq1 && cnt1 > nums.size()/3){
-        //     return {ele, ele1};
-        // }
-        // if(cnt > nums.size()/3 && freq){
-        //     return {ele};
-        // }
-        // if(cnt1 > nums.size()/3 && freq1){
-        //     return {ele1};
-        // }
-        // return {};
+        //TC: O(n);
+        
+        int n = nums.size();
+        int freq1 = 0;
+        int freq2 = 0;
+        int ele1 = nums[0];
+        int ele2 = nums[0];
+
+        for(int i = 0; i < n; i++){
+            if(ele1 == nums[i]){
+                freq1++;
+            }
+            else if(ele2 == nums[i]){
+                freq2++;
+            }
+            else if(freq1 == 0){
+                ele1 = nums[i];
+                freq1 = 1;
+            }
+            else if(freq2 == 0){
+                ele2 = nums[i];
+                freq2 = 1;
+            }
+            else{
+                freq1--;
+                freq2--;
+            }
+        }
+        if(ele1 == ele2) return {ele1};
+
+        freq1 = 0;
+        freq2 = 0;
+        for(int i : nums){
+            if(i == ele1) freq1++;
+            if(i == ele2) freq2++;
+        }
+        vector<int> ans;
+        if(freq1 > n/3) ans.push_back(ele1);
+        if(freq2 > n/3) ans.push_back(ele2);
+        return ans;
     }
 };
