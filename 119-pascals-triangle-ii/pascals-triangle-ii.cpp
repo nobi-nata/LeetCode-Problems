082b9fd1f76bbb7e8856aeb3794e5e5d8f86e9a4
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-11 16:31:39 +0530

    Time: 3 ms (37.08%) | Memory: 6.9 MB (22.42%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<int> getRow(int rowIndex) {
-         vector<vector<int>>dp;
+      vector<int> dp;
       dp.push_back({1});
       for(int i=1;i<=rowIndex;i++){
-       vector<int>prevvectvec = dp[i-1];
-        vector<int>currvect;
+        vector<int> currvect;
         currvect.push_back(1);
-        for(int i=1;i<prevvectvec.size();i++){
-            currvect.push_back(prevvectvec[i-1]+prevvectvec[i]);
+        for(int i=1;i<dp.size();i++){
+            currvect.push_back(dp[i-1]+dp[i]);
         }
         currvect.push_back(1);
-        dp.push_back(currvect);
+        dp = currvect;
       }
-    return dp[rowIndex];
-    
+      return dp;
+
     }
 };
