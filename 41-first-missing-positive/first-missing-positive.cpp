467828ad45a2f35ddf4b35a7071c1d7aca078b7d
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-12 12:25:48 +0530

    Time: 39 ms (95.79%) | Memory: 41.7 MB (28.79%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -16,49 +16,49 @@ public:
 
         //TC: O(nlogn)
 
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        auto it = unique(nums.begin(), nums.end());
-
-        nums.resize(distance(nums.begin(), it));
-        int j = 0;
-        int i = 1;
-        while(j < n){
-            while(j < n && nums[j] < 1){
-                j++;
-            }
-            if(j == n) break;
-            if(nums[j] == i){
-                cout << 'y';
-                i++;
-            }
-            else return i;
-            j++;
-        }
-        return i;
-        
-        
         // int n = nums.size();
-        // int i = 0;
-        // while(i < n){
-        //     if(nums[i] == (i+1) || nums[i] <= 0 || nums[i] > n){
-        //         i++;
-        //         continue;
+        // sort(nums.begin(), nums.end());
+        // auto it = unique(nums.begin(), nums.end());
+
+        // nums.resize(distance(nums.begin(), it));
+        // int j = 0;
+        // int i = 1;
+        // while(j < n){
+        //     while(j < n && nums[j] < 1){
+        //         j++;
         //     }
+        //     if(j == n) break;
+        //     if(nums[j] == i){
+        //         cout << 'y';
+        //         i++;
+        //     }
+        //     else return i;
+        //     j++;
+        // }
+        // return i;
+        
+        
+        int n = nums.size();
+        int i = 0;
+        while(i < n){
+            if(nums[i] == (i+1) || nums[i] <= 0 || nums[i] > n){
+                i++;
+                continue;
+            }
             
-        //     int idx1 = i;
-        //     int idx2 = nums[i] - 1;
-        //     if(nums[idx1] == nums[idx2]){
-        //         i++;
-        //         continue;
-        //     }
-        //     swap(nums[idx1],nums[idx2]);
-        // }
-        // for(int i = 0; i < n; i++){
-        //     if(nums[i] != i+1){
-        //         return i+1;
-        //     }
-        // }
-        // return n+1;
+            int idx1 = i;
+            int idx2 = nums[i] - 1;
+            if(nums[idx1] == nums[idx2]){
+                i++;
+                continue;
+            }
+            swap(nums[idx1],nums[idx2]);
+        }
+        for(int i = 0; i < n; i++){
+            if(nums[i] != i+1){
+                return i+1;
+            }
+        }
+        return n+1;
     }
 };
