67dc2c6dce974101e2b543cbe435812e10feddf7
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-12 18:54:51 +0530

    Time: 11 ms (64.81%) | Memory: 20.3 MB (42.21%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -16,49 +16,52 @@ public:
         //     }
         //     int h = min(left, right);
         //     int w = h - height[i];
-        //     if(w > 0) ans += w;
+        //     if(w > 0) ans += w;  // if if(w>0) is to be removed change in for condition j = i instead of i-1 & i+1;
         // }
         // return ans;
 
         //TC: O(n) SC: O(n)
 
-        int n = height.size();
-        vector<int> prefix(n, 0);
-        vector<int> suffix(n , 0);
-        prefix[0] = height[0];
-        suffix[n-1] = height[n-1];
+        // int n = height.size();
+        // vector<int> prefix(n, 0);
+        // vector<int> suffix(n , 0);
+        // prefix[0] = height[0];
+        // suffix[n-1] = height[n-1];
 
-        for(int i = 1; i < n; i++){
-            prefix[i] = max(prefix[i-1], height[i]);
-            suffix[n-i-1] = max(suffix[n-i], height[n-i-1]);
-        }
-        int ans = 0;
-        for(int i= 0; i < n; i++){
-            ans += min(prefix[i], suffix[i]) - height[i];
-        }
-        return ans;
-
-        // int i = 0;
-        // int j = n -1;
-            
-        // int prefix = 0;
-        // int suffix = 0;
-        // int ans = 0;
-
-        // while(i < j){
-        //     prefix = max(prefix, height[i]);
-        //     suffix = max(suffix, height[j]);
-
-        //     if(prefix <= suffix){
-        //         ans += prefix - height[i];
-        //         i++;
-        //     }
-        //     else{
-        //         ans += suffix - height[j];
-        //         j--;
-        //     }
+        // for(int i = 1; i < n; i++){
+        //     prefix[i] = max(prefix[i-1], height[i]);
+        //     suffix[n-i-1] = max(suffix[n-i], height[n-i-1]);
         // }
-
+        // int ans = 0;
+        // for(int i= 0; i < n; i++){
+        //     ans += min(prefix[i], suffix[i]) - height[i];
+        // }
         // return ans;
+
+        //TC: O(n)
+
+        int n = height.size();
+        int i = 0;
+        int j = n -1;
+            
+        int prefix = 0;
+        int suffix = 0;
+        int ans = 0;
+
+        while(i < j){
+            prefix = max(prefix, height[i]);
+            suffix = max(suffix, height[j]);
+
+            if(prefix <= suffix){
+                ans += prefix - height[i];
+                i++;
+            }
+            else{
+                ans += suffix - height[j];
+                j--;
+            }
+        }
+
+        return ans;
     }
 };
