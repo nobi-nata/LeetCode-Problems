0e89d51c77d51d2f5c49699a75a5b11640f97944
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-13 17:49:40 +0530

    Time: 56 ms (95.81%) | Memory: 59.3 MB (41.36%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
+        //TC: O(n^2)
+        // int ans = 0;
+        // int n = height.size();
+        // for(int i = 0; i < n; i++){
+        //     int leftmax = INT_MIN;
+        //     int ind = 0;
+        //     for(int j = i+1; j < n; j++){
+        //         ans = max(ans, (j-i)*min(height[i], height[j]));
+        //     }            
+        // }
+        // return ans;
+
+
+        //TC: O(n)
+
         int n = height.size();
         int i = 0;
         int j = n-1;
