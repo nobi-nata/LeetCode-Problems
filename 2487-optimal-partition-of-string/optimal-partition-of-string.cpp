5e1e337912ecd6fe4cc7fb0564ef46d0b4e74ac3
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-14 18:24:39 +0530

    Time: 19 ms (90.80%) | Memory: 10.5 MB (74.33%) - LeetSync

--- 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
+++ 2487-optimal-partition-of-string/optimal-partition-of-string.cpp
@@ -1,14 +1,29 @@
 class Solution {
 public:
     int partitionString(string s) {
-        unordered_map<char, int> mp;
+        // unordered_map<char, int> mp;
+        // int cnt = 1;
+        // for(int i = 0; i  <s.size(); i++){
+        //     if(mp.find(s[i]) != mp.end()){
+        //         cnt++;
+        //         mp.clear();
+        //     }
+        //     mp[s[i]]++;
+        // }
+        // return cnt;
+
+        int feq[26] = {0};
+        int l = 0, r = 0, n = s.length();
         int cnt = 1;
-        for(int i = 0; i  <s.size(); i++){
-            if(mp.find(s[i]) != mp.end()){
+        for(; r < n; r++){
+            int ch = s[r] - 'a';
+            feq[ch]++;
+            if(feq[ch] > 1){
                 cnt++;
-                mp.clear();
+                while(l < r){
+                    feq[s[l++] - 'a']--;
+                }
             }
-            mp[s[i]]++;
         }
         return cnt;
     }
