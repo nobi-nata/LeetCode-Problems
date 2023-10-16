31b5b4f094bedc8b9f7a2a11e82673fc0b79a58c
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-16 12:47:45 +0530

    Time: 15 ms (62.12%) | Memory: 17.2 MB (51.25%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -16,19 +16,28 @@ public:
 
         //TC: O(nlogn)
         
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+        // int n = nums.size();
+        // if(n == 1) return nums[0];
+        // for(int i = 0; i < n; i++){
+        //     if(i == 0){
+        //         if(nums[i] != nums[i+1])
+        //             return nums[i];}
+        //     else if(i == n-1){
+        //         if(nums[i] != nums[i-1])
+        //             return nums[i];}
+        //     else if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) return nums[i];
+        // }
+        // return 0;
+
+        //TC: O(n)
         int n = nums.size();
-        if(n == 1) return nums[0];
+        int ans = 0;
         for(int i = 0; i < n; i++){
-            if(i == 0){
-                if(nums[i] != nums[i+1])
-                    return nums[i];}
-            else if(i == n-1){
-                if(nums[i] != nums[i-1])
-                    return nums[i];}
-            else if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) return nums[i];
+            ans ^= nums[i];
         }
-        return 0;
+        return ans;
+        
 
     }
 };
