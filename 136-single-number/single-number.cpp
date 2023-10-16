84feffb22aa79d3a78aca3c3d2172012193c07fb
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-16 12:42:22 +0530

    Time: 25 ms (17.91%) | Memory: 17.2 MB (51.25%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        for(auto i : mp){
-            if(i.second == 1) return i.first;
+        // TC: O(n) SC: O(n)
+
+        // unordered_map<int, int> mp;
+        // for(int i = 0; i < nums.size(); i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto i : mp){
+        //     if(i.second == 1) return i.first;
+        // }
+        // return 0;
+
+
+
+        //TC: O(nlogn)
+        
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        for(int i = 0; i < n; i++){
+            if(i == 0){
+                if(nums[i] != nums[i+1])
+                    return nums[i];}
+            else if(i == n-1){
+                if(nums[i] != nums[i-1])
+                    return nums[i];}
+            else if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) return nums[i];
         }
         return 0;
+
     }
 };
