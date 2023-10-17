db540dc72a3159d210da713feeb85cccb120425d
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-17 23:06:07 +0530

    Time: 65 ms (77.20%) | Memory: 41.8 MB (71.07%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -14,5 +14,33 @@ public:
             mp[sum]++;
         }
         return ans;
+
+        // int n = nums.size();
+        // int ans = 0;
+
+        // for(int i = 0; i < n; i++){
+        //     for(int j = i; j < n; j++){
+        //         int sum = 0;
+        //         for(int k = i; k <= j; k++){
+        //             sum += nums[k];
+        //         }
+        //         if(sum == k) ans++;
+        //     }
+        // }
+        // return ans;
+
+        // int n = nums.size();
+        // int ans = 0;
+
+        // for(int i = 0; i < n; i++){
+        //     int sum = 0;
+        //     for(int j = i; j < n; j++){
+        //         sum += nums[j];
+        //         if(sum == k) ans++;
+        //     }
+        // }
+        // return ans;
+
+    
     }
 };
