91bbe3d8d9f1bf5fe5d76abb62c8d8196bf4ddaf
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-17 22:33:35 +0530

    Time: 30 ms (91.21%) | Memory: 22.2 MB (40.37%) - LeetSync

--- 554-brick-wall/brick-wall.cpp
+++ 554-brick-wall/brick-wall.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int leastBricks(vector<vector<int>>& wall) {
+        unordered_map<int, int> mp;
+
         int n = wall.size();
         int ans = 0;
-        unordered_map<int, int> mp;
-        for(auto r : wall){
-            int prefix  = 0;
-
-            for(int i = 0; i < r.size() - 1; i++){
-                prefix += r[i];
-                mp[prefix]++;
-                ans = max(ans, mp[prefix]);
+        for(int i = 0; i < n; i++){
+            int sum = 0;
+            for(int j = 0; j < wall[i].size()-1; j++){
+                sum += wall[i][j];
+                mp[sum]++;
+                ans = max(ans, mp[sum]);
             }
         }
         return n - ans;
