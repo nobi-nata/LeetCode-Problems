9bdeea1d76253b5e7c9aee5ab918454444f603ea
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-18 06:44:27 +0530

    Time: 165 ms (87.26%) | Memory: 97.7 MB (7.49%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.cpp
@@ -1,35 +1,60 @@
 class RandomizedSet {
 public:
-    unordered_set<int> st;
+    unordered_map<int, int> mp;
+    vector<int> v;
+    int ind = 0;
     RandomizedSet() {
-        
+        ind = 0;
+        v.clear();
+        mp.clear();
     }
     
     bool insert(int val) {
-        if(st.find(val) != st.end()){
-            return false;
-        }
-        else{
-            st.insert(val);
-            return true;
-        }
+        // if(st.find(val) != st.end()){
+        //     return false;
+        // }
+        // else{
+        //     st.insert(val);
+        //     return true;
+        // }
+
+        if(mp.find(val) != mp.end()) return false;
+
+        mp[val] = ind++;
+        v.push_back(val);
+        return true;
     }
     
     bool remove(int val) {
-        if(st.find(val) != st.end()){
-            st.erase(val);
-            return true;
-        }
-        else{
-            return false;
-        }
+        // if(st.find(val) != st.end()){
+        //     st.erase(val);
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
+
+
+        if(mp.find(val) == mp.end()) return false;
+
+        int i = mp[val];
+        swap(v[i], v[ind-1]);
+        mp[v[i]] = i;
+
+        v.pop_back();
+        ind--;
+        mp.erase(val);
+        return true;
     }
     
     int getRandom() {
-        int r = rand()%st.size();
-        auto it = st.begin();
-        advance(it, r);
-        return *(it);
+        // int r = rand()%st.size();
+        // auto it = st.begin();
+        // advance(it, r);
+        // return *(it);
+
+        int r = rand()%ind;
+        return v[r];
     }
 };
 
