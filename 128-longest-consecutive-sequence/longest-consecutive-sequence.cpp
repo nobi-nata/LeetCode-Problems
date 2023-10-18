b169c56bc3b11e1b92cbc12cef954912f48bd731
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-18 08:05:58 +0530

    Time: 307 ms (29.60%) | Memory: 76.7 MB (5.47%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,25 +1,46 @@
 class Solution {
 public:
-    int longestConsecutive(vector<int>& a) {
-        int n = a.size();
-        int longest = 1;
-        if(n == 0) return 0;
-        unordered_set<int> st;
-        for(int i = 0; i < n; i++){
-            st.insert(a[i]);
+    int longestConsecutive(vector<int>& nums) {
+        //TC:O(n^3)
+
+        // int n = nums.size();
+        // int ans = 0;
+        // for(int i = 0; i < n; i++){
+        //     int cnt = 1;
+        //     for(int j = 0; j < n; j++){
+        //         for(int k = 0; k < n; k++){
+        //             if(nums[i] + cnt == nums[k]){ 
+        //                 cnt++;
+        //                 break;
+        //             }
+        //         }
+        //     }
+        //     ans = max(ans, cnt);
+        // }
+        // return ans;
+
+        int n = nums.size();
+        set<int> st;
+        for(int i : nums){
+            st.insert(i);
         }
+        vector<int> num;
         for(auto i : st){
-            if(st.find(i-1) == st.end()){
-                int cnt = 1;
-                int x = i;
-                while(st.find(x+1) != st.end()){
-                    cnt++;
-                    x++;
-                }
-                longest = max(longest, cnt);
-            }
-            
+            num.push_back(i);
         }
-        return longest;
+
+        int ans = 0;
+        if(n == 0) return 0;
+        int cnt = 1;
+        for(int i = 1; i < num.size(); i++){
+            if(num[i] == num[i-1]+1) cnt++;
+            else{
+                ans = max(ans, cnt);
+                cnt = 1;
+            }
+            cout << cnt << " "; 
+        }
+        ans = max(ans, cnt);
+        return ans;
     }
 };
