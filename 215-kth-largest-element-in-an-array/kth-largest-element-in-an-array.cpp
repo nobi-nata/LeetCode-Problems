f2ef4eeea8d2d7b72ed3cd87786b22ed8f010f0e
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-20 17:44:36 +0530

    Time: 58 ms (98.96%) | Memory: 56.7 MB (65.08%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -4,17 +4,39 @@ public:
         // sort(nums.rbegin(), nums.rend());
         // return nums[k-1];
 
-        priority_queue<int, vector<int> , greater<int>> pq;
+        // priority_queue<int, vector<int> , greater<int>> pq;
+
+        // for(int i = 0; i < nums.size(); i++){
+        //     if(pq.size() < k){
+        //         pq.push(nums[i]);
+        //     }
+        //     else if(nums[i] > pq.top()){
+        //         pq.pop();
+        //         pq.push(nums[i]);
+        //     }
+        // }
+        // return pq.top();
+
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        for(int i = 0; i < nums.size(); i++){
+            mini = min(nums[i], mini);
+            maxi = max(maxi, nums[i]);
+        }
+
+        vector<int> cnt(maxi-mini+1, 0);
 
         for(int i = 0; i < nums.size(); i++){
-            if(pq.size() < k){
-                pq.push(nums[i]);
-            }
-            else if(nums[i] > pq.top()){
-                pq.pop();
-                pq.push(nums[i]);
+            cnt[nums[i] - mini] += 1;
+        }
+        int rem = k;
+        for(int i = cnt.size()-1; i >= 0; i--){
+            rem -= cnt[i];
+            if(rem <= 0){
+                return i+mini;
             }
         }
-        return pq.top();
+        return 0;
+
     }
 };
