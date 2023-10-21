e41c55757a31333484fc28b74fa11c5205f5b02d
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-21 08:39:44 +0530

    Time: 280 ms (46.73%) | Memory: 144.7 MB (30.20%) - LeetSync

--- 1286-constrained-subsequence-sum/constrained-subsequence-sum.cpp
+++ 1286-constrained-subsequence-sum/constrained-subsequence-sum.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
-        std::deque<int> dq;
-        for (int i = 0; i < nums.size(); ++i) {
-            nums[i] += !dq.empty() ? nums[dq.front()] : 0;
+        // std::deque<int> dq;
+        // for (int i = 0; i < nums.size(); ++i) {
+        //     nums[i] += !dq.empty() ? nums[dq.front()] : 0;
             
-            while (!dq.empty() && (i - dq.front() >= k || nums[i] >= nums[dq.back()])) {
-                if (nums[i] >= nums[dq.back()]) dq.pop_back();
-                else dq.pop_front();
-            }
+        //     while (!dq.empty() && (i - dq.front() >= k || nums[i] >= nums[dq.back()])) {
+        //         if (nums[i] >= nums[dq.back()]) dq.pop_back();
+        //         else dq.pop_front();
+        //     }
             
-            if (nums[i] > 0) {
-                dq.push_back(i);
+        //     if (nums[i] > 0) {
+        //         dq.push_back(i);
+        //     }
+        // }
+        // return *std::max_element(nums.begin(), nums.end());
+
+        int n = nums.size();
+        priority_queue<pair<int, int>> pq;
+        vector<int> dp(n, 0);
+
+        pq.push({nums[0], 0});
+        int ans = nums[0];
+        dp[0] = nums[0];
+
+        for(int i = 1; i < nums.size(); i++){
+            while(i - pq.top().second > k){
+                pq.pop();
             }
+
+            dp[i] = max(nums[i], nums[i] + pq.top().first);
+            ans = max(ans, dp[i]);
+            pq.push({dp[i], i});
         }
-        return *std::max_element(nums.begin(), nums.end());
+        return ans;
     }
 };
