7c5bed5fc76180798f44ed4fe9135d523e7a6e5f
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-23 08:26:21 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (62.77%) - LeetSync

--- 342-power-of-four/power-of-four.cpp
+++ 342-power-of-four/power-of-four.cpp
@@ -1,20 +1,14 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        if(n%4 || n == INT_MIN){
-            cout <<'-';
-            if(n == 1) return true;
-            return false;
+        if(n < 0) return false;
+        if(n == 1) return true;
+        int ele = n;
+        while(ele > 4 && ele%4 == 0){
+            ele /= 4;
         }
-        else{
-            
-            if(n < 0) return false;
-            int ele = n;
-            while(ele > 4 && ele%4 == 0){
-                ele /= 4;
-            }
-            if(ele == 4) return true;
-        }
+        if(ele == 4) return true;
+        
         return false;
     }
 };
