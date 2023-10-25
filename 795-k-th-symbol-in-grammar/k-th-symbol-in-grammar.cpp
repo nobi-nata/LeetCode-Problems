0ab977fc6fe95530f74e3eebde27ba844e329b39
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-10-25 07:25:30 +0530

    Time: 2 ms (51.37%) | Memory: 6.2 MB (53.37%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.cpp
@@ -21,9 +21,11 @@ public:
         // }
         // return prev[k-1];
 
-        if (n == 1) return 0;
-        int length = 1 << (n - 2);
-        if (k <= length) return kthGrammar(n - 1, k);
-        else return 1 - kthGrammar(n - 1, k - length);
+        // if (n == 1) return 0;
+        // int length = 1 << (n - 2);
+        // if (k <= length) return kthGrammar(n - 1, k);
+        // else return 1 - kthGrammar(n - 1, k - length);
+
+        return __builtin_popcount(k-1) &1 ;
     }
 };
