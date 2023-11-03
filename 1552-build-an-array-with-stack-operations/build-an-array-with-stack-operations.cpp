a2c6d3cdc59b54d7e944fbf1a6fbccb2c2f28f88
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-11-03 11:20:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (75.36%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -1,20 +1,41 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        int i = 0;
-        vector<string> ans;
-        int ele = target[target.size()-1];
-        for(int num = 1; num <= n; num++){
-            ans.push_back("Push");
-            if(i < target.size() && target[i] != num){
-                ans.push_back("Pop");
-            }
-            else{
-                i++;
-            }
-            if(ele == num) break;
+        // int i = 0;
+        // vector<string> ans;
+        // int ele = target[target.size()-1];
+        // for(int num = 1; num <= n; num++){
+        //     ans.push_back("Push");
+        //     if(i < target.size() && target[i] != num){
+        //         ans.push_back("Pop");
+        //     }
+        //     else{
+        //         i++;
+        //     }
+        //     if(ele == num) break;
             
+        // }
+        // return ans;
+
+
+
+        vector<string> result;
+        int current = 1; // The current number to be pushed.
+
+        for (int i = 0; i < target.size(); i++) {
+            while (current < target[i]) {
+                // While the current number is less than the target number,
+                // push the current number and generate the "Push" operation.
+                result.push_back("Push");
+                result.push_back("Pop"); // After pushing, immediately pop.
+                current++;
+            }
+
+            // The current number matches the target number, so push it.
+            result.push_back("Push");
+
+            current++; // Move to the next number to be pushed.
         }
-        return ans;
+        return result;
     }
 };
