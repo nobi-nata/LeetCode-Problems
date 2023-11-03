2dfc7d4e42689cc894e94b0f700e51f10677f39f
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-11-03 11:24:09 +0530

    Time: 5 ms (39.00%) | Memory: 8.2 MB (49.67%) - LeetSync

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -19,23 +19,46 @@ public:
 
 
 
-        vector<string> result;
-        int current = 1; // The current number to be pushed.
+        // vector<string> result;
+        // int current = 1; // The current number to be pushed.
 
-        for (int i = 0; i < target.size(); i++) {
-            while (current < target[i]) {
-                // While the current number is less than the target number,
-                // push the current number and generate the "Push" operation.
-                result.push_back("Push");
-                result.push_back("Pop"); // After pushing, immediately pop.
-                current++;
+        // for (int i = 0; i < target.size(); i++) {
+        //     while (current < target[i]) {
+        //         // While the current number is less than the target number,
+        //         // push the current number and generate the "Push" operation.
+        //         result.push_back("Push");
+        //         result.push_back("Pop"); // After pushing, immediately pop.
+        //         current++;
+        //     }
+
+        //     // The current number matches the target number, so push it.
+        //     result.push_back("Push");
+
+        //     current++; // Move to the next number to be pushed.
+        // }
+        // return result;
+
+
+
+         vector<string> operations;
+        int targetIndex = 0; // Pointer for target array
+        int currentNumber = 1; // Pointer for numbers 1 to n
+
+        while (targetIndex < target.size()) {
+            if (target[targetIndex] == currentNumber) {
+                // If the current number in the target array matches the current number 1 to n
+                // Push "Push" operation and move both pointers
+                operations.push_back("Push");
+                targetIndex++;
+            } else {
+                // If the numbers don't match, push "Push" followed by "Pop" operation
+                // and only move the current number pointer
+                operations.push_back("Push");
+                operations.push_back("Pop");
             }
-
-            // The current number matches the target number, so push it.
-            result.push_back("Push");
-
-            current++; // Move to the next number to be pushed.
+            currentNumber++;
         }
-        return result;
+
+        return operations;
     }
 };
