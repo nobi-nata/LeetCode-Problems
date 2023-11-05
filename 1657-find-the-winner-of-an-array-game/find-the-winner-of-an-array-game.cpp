5a3804e441263d8ed0dc97fdb3d90fa9f8ce4c13
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-11-05 15:57:09 +0530

    Time: 71 ms (95.83%) | Memory: 63.5 MB (40.28%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
@@ -1,19 +1,45 @@
 class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
-        int n = arr.size();
-        if(k >= n-1) return *max_element(arr.begin(), arr.end());
+        // int n = arr.size();
+        // if(k >= n-1) return *max_element(arr.begin(), arr.end());
 
-        unordered_map<int,int> mp;
+        // unordered_map<int,int> mp;
 
-        for(int i = 1; i < n; i++){
-            if(arr[i] > arr[0]){
-                swap(arr[i], arr[0]); 
-            }
-            mp[arr[0]]++;
-            if(mp[arr[0]] >= k) return arr[0];
+        // for(int i = 1; i < n; i++){
+        //     if(arr[i] > arr[0]){
+        //         swap(arr[i], arr[0]); 
+        //     }
+        //     mp[arr[0]]++;
+        //     if(mp[arr[0]] >= k) return arr[0];
+        // }
+
+        // return arr[0];
+
+
+         if (k == 1) {
+            return std::max(arr[0], arr[1]);
+        }
+        if (k >= arr.size()) {
+            return *std::max_element(arr.begin(), arr.end());
         }
 
-        return arr[0];
+        int current_winner = arr[0];
+        int consecutive_wins = 0;
+
+        for (int i = 1; i < arr.size(); ++i) {
+            if (current_winner > arr[i]) {
+                consecutive_wins++;
+            } else {
+                current_winner = arr[i];
+                consecutive_wins = 1;
+            }
+
+            if (consecutive_wins == k) {
+                return current_winner;
+            }
+        }
+
+        return current_winner;
     }
 };
