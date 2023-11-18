99eddedba53b9c2db4f57e2d1890428cb9c6bcac
Laxmikant Gurav <85544667+nobi-nata@users.noreply.github.com>
2023-11-18 07:01:54 +0530

    Time: 141 ms (96.67%) | Memory: 107.7 MB (7.36%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,26 +1,87 @@
+// class Solution {
+// public:
+//     int maxFrequency(vector<int>& nums, int k) {
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+        
+//         int j = 0;
+//         int i = 0;
+//         long sum = 0;
+//         long size = 0;
+//         long ans = 0;
+//         while(i < n){
+//             size = i-j+1;
+//             sum += nums[i];
+
+//             while((nums[i]*size - sum) > k){
+//                 size--;
+//                 sum -= nums[j];
+//                 j++;
+//             }
+//             ans = max(ans, size);
+//             i++;
+//         }
+//         return ans;
+//     }
+// };
+
+
+
+
 class Solution {
 public:
-    int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        
-        int j = 0;
-        int i = 0;
-        long sum = 0;
-        long size = 0;
-        long ans = 0;
-        while(i < n){
-            size = i-j+1;
-            sum += nums[i];
+    int maxFrequency(std::vector<int>& nums, int k) {
+        countingSort(nums); // Sort the array using counting sort
+        int start = 0; // Start index of the current subarray
+        long long subarraySum = 0; // Sum of elements in the current subarray
+        int maxFrequency = 1; // Initialize the maximum frequency to 1
 
-            while((nums[i]*size - sum) > k){
-                size--;
-                sum -= nums[j];
-                j++;
+        // Iterate through the sorted array
+        for (int i = 0; i < nums.size(); i++) {
+            int subarrayLength = i - start + 1; // Length of the current subarray
+            long long subarrayProduct = (long long)nums[i] * subarrayLength; // Use long long to avoid overflow
+            subarraySum += nums[i]; // Add the current element to the subarray sum
+
+            // Adjust the subarray to satisfy the condition (product - sum <= k)
+            while (subarrayProduct - subarraySum > k) {
+                subarraySum -= nums[start]; // Remove the leftmost element from the subarray
+                start++; // Move the start index to the right
+                subarrayLength--; // Decrease the length of the subarray
+                subarrayProduct = (long long)nums[i] * subarrayLength; // Recalculate the product
             }
-            ans = max(ans, size);
-            i++;
+
+            // Update the maximum frequency based on the current subarray length
+            maxFrequency = std::max(maxFrequency, subarrayLength);
         }
-        return ans;
+
+        return maxFrequency; // Return the final result
+    }
+
+private:
+    // Counting Sort to sort the array in non-decreasing order
+    void countingSort(std::vector<int>& nums) {
+        int maxElement = INT_MIN;
+
+        // Find the maximum element in the array
+        for (int num : nums) {
+            maxElement = std::max(maxElement, num);
+        }
+
+        // Create an array to store the count of each element
+        std::vector<int> countMap(maxElement + 1, 0);
+
+        // Count the occurrences of each element in the array
+        for (int num : nums) {
+            countMap[num]++;
+        }
+
+        int index = 0;
+
+        // Reconstruct the sorted array
+        for (int i = 0; i <= maxElement; i++) {
+            while (countMap[i]-- > 0) {
+                nums[index++] = i;
+            }
+        }
     }
 };
